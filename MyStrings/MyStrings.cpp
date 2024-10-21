3b2d40759cb4d949bff92eb607fbd8c48925f5e0
Bortnik <bortnikyarik@gmail.com>
2024-10-21 05:11:37 +0300

    Добавьте файлы проекта.

---
+++ MyStrings/MyStrings.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "MyString.h"
+
+using namespace std;
+
+int main() 
+{
+	MyString stringExample("BARROWS");
+	cout << "Len = " << stringExample.MyStrLen() << endl;
+	stringExample.Print();
+
+	stringExample.Input();
+	stringExample.Print();
+
+	MyString stringExample2("SKOFF");
+
+	stringExample += " Amigo!";
+	stringExample.Print();
+
+	MyString stringExample3;
+	stringExample3 = "Holla, " + stringExample2;
+	stringExample3.Print();
+
+	MyString::GetCountLiveObj();
+	MyString::GetCountCreatedObj();
+
+	MyString str{ 'H', 'o', 'l', 'a', ',', ' ', 'A', 'm', 'i', 'g', 'o', '!' };
+	str.Print();
+
+	return 0;
+}
---
+++ MyStrings/myString.h
@@ -0,0 +1,44 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class MyString
+{
+	char* str;
+	int length;
+	static int countLiveObj;
+	static int countCreatedObj;
+public:
+	MyString();
+	MyString(int size);
+	MyString(const char* initStr);
+	MyString(const MyString& other);
+	MyString(MyString&& obj);
+	MyString& operator=(MyString&& obj);
+	MyString(initializer_list<char> a);
+	~MyString();
+
+	void MyStrcpy(MyString& obj);
+	bool MyStrStr(const char* substr);
+	int MyChr(char c);
+	int MyStrLen();
+	void MyStrCat(MyString& b);
+	void MyDelChr(char c);
+	int MyStrCmp(MyString& b);
+
+	MyString& operator+=(const char* appendStr);
+
+	void Print();
+	void Input();
+
+	static void GetCountLiveObj();
+	static void GetCountCreatedObj();
+
+	MyString operator=(const MyString& obj);
+	MyString operator[](int index);
+	void operator()();
+	friend ostream& operator<<(ostream& os, MyString obj);
+	friend istream& operator>>(istream& is, MyString& obj);
+};
+ostream& operator<<(ostream& os, MyString obj);
+istream& operator>>(istream& is, MyString& obj);
+MyString operator+(const char* prefixStr, const MyString& myStr);
